a33b01bf8b277fdc146af6b7c296a427bf45a1d8
Sheldon Robinson <sheldon.robinson@gmail.com>
2025-03-15 01:32:48 -0400

    Adding packaged extensions

---
+++ extension/extension_config_local.cmake
@@ -0,0 +1,18 @@
+duckdb_extension_load(icu)
+duckdb_extension_load(json)
+
+################# FTS
+duckdb_extension_load(fts)
+
+################# VSS
+duckdb_extension_load(vss)
+
+
+################# SQLITE
+duckdb_extension_load(sqlite)
+
+################# SPATIAL
+# duckdb_extension_load(spatial
+        # GIT_URL https://github.com/duckdb/duckdb-spatial
+        # GIT_TAG 6b3d93c3ef6bffa7b2453963ef438fc5bb9536e1
+# )
